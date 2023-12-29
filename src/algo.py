cf3622c4fe01b2957ac3f5a440b260e7050193bc
zong006 <ongzhicong@gmail.com>
2023-12-29 13:32:02 +0800

    update to print feature importance

--- src/algo.py
+++ src/algo.py
@@ -1,12 +1,12 @@
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
 def randomforestclassifier(X_train, y_train, X_test, y_test):
-    classifier = RandomForestClassifier(random_state=42)
+    from sklearn.ensemble import RandomForestClassifier
+    clf = RandomForestClassifier(random_state=42)
 
 
-    classifier.fit(X_train, y_train)
-    y_pred = classifier.predict(X_test)
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
 
     accuracy = accuracy_score(y_test, y_pred)
     conf_matrix = confusion_matrix(y_test, y_pred)
@@ -18,4 +18,25 @@ def randomforestclassifier(X_train, y_train, X_test, y_test):
     print("Confusion Matrix:")
     print(conf_matrix)
     
-    return classifier
+    return clf
+
+
+def logregclassifier(X_train, y_train, X_test, y_test):
+    from sklearn.linear_model import LogisticRegression
+
+    clf = LogisticRegression(C=3)
+    clf.fit(X_train, y_train)
+    
+    y_pred = clf.predict(X_test)
+
+    accuracy = accuracy_score(y_test, y_pred)
+    conf_matrix = confusion_matrix(y_test, y_pred)
+    classification_report_str = classification_report(y_test, y_pred)
+
+    print(f"Accuracy: {accuracy:.2f}")
+    print("\nClassification Report:\n", classification_rep)
+
+    print("Confusion Matrix:")
+    print(conf_matrix)
+
+    return clf
--- src/main.py
+++ src/main.py
@@ -1,25 +1,32 @@
 from data_extraction import extract_data
 from data_preprocessing import pre_processing_data
-from algo import randomforestclassifier
+from algo import randomforestclassifier, logregclassifier
 import pandas as pd
 
 def main():
     extracted_data = extract_data()
     X_train, y_train, X_test, y_test = pre_processing_data(extracted_data)
 
-    classifier = randomforestclassifier(X_train, y_train, X_test, y_test)
+    clf_randomforest = randomforestclassifier(X_train, y_train, X_test, y_test)
+    clf_logreg = logregclassifier(X_train, y_train, X_test, y_test)
     
-    feature_importances = classifier.feature_importances_
     X = extracted_data.drop(['class'], axis=1)
-    feature_importance_df = pd.DataFrame({
+
+    feature_importances_rfor = clf_randomforest.feature_importances_
+    feature_importance_df_rfor = pd.DataFrame({
         'Feature': X.columns,
-        'Importance': feature_importances
+        'Importance': feature_importances_rfor
     })
 
+    feature_importance_df_rfor = feature_importance_df_rfor.sort_values(by='Importance', ascending=False)
+    print("Feature Importance of random forest:\n", feature_importance_df_rfor)
 
-    feature_importance_df = feature_importance_df.sort_values(by='Importance', ascending=False)
-    print("Feature Importance:\n", feature_importance_df)
-    
+    coefficients = clf_logreg.coef_[0]
+    df_coefficients = pd.DataFrame({'Feature': X.columns, 'Coefficient': coefficients})
+    df_coefficients = df_coefficients.reindex(df_coefficients['Coefficient'].abs().sort_values(ascending=False).index)
+    df_coefficients['Odds Ratio'] = df_coefficients['Coefficient'].apply(lambda x: round(np.exp(x), 4))
+
+    print("Feature Importance of logistic regression:\n",df_coefficients.head(10))
     return
 
 
